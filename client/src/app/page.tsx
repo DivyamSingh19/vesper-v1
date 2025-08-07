7b66bd64e28191de10751c34900caabad61fdce6
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-08-07 17:00:56 +0530

    stuff

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,18 @@
 import Hero from "@/components/Landing/Hero";
 import React from "react";
-
+import FeaturesCard from "@/components/Landing/features"
 function page() {
   return (
-    <div>
-      landing
-    </div>
+    <>
+      {/* Header */}
+      <Hero
+        title="All your Business Expenses in one place"
+        subTitle="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."
+        // backgroundImg="/images/dg.jpg"
+      />
+    <FeaturesCard/>
+    </>
+    
   );
 }
 
