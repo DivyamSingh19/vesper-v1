47ed08c67b78bea92e8d590ff7f2dff520003b79
Divyam Singh <divyamsingh1918@gmail.com>
2025-08-09 13:07:59 +0530

    updates

--- client/src/app/trial/page.tsx
+++ client/src/app/trial/page.tsx
@@ -1,9 +1,10 @@
+import { AiChat } from '@/components/core/AiChat'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      
+      <AiChat/>
     </div>
   )
 }
---
+++ client/src/components/core/AiChat.tsx
@@ -0,0 +1,9 @@
+import { AnimatedAIChat } from "@/components/ui/ai-animated-code"
+
+export function AiChat() {
+  return (
+    <div className="flex w-screen overflow-x-hidden">
+      <AnimatedAIChat />
+    </div>
+  );
+}
