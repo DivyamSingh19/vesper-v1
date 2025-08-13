59970d1fb945be3364759efccebc597dacd5d073
Divyam Singh <divyamsingh1918@gmail.com>
2025-08-13 19:42:39 +0530

    auth-done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono'
+import authRoutes from './routes/auth.route'
+import { cors } from 'hono/cors'
 
-const app = new Hono()
 
+export type Env = {
+  DATABASE_URL:string
+}
+
+const app = new Hono<{Bindings:Env}>()
+app.use('*',cors())
 app.get('/', (c) => {
+  c.env.DATABASE_URL
   return c.text('API working!')
 })
 
+
+app.route('/api/v1/auth',authRoutes)
 export default app
