72f7889aba8bbb2128f6cae83d858cfb42717fe5
focusedaf <126765236+focusedaf@users.noreply.github.com>
2025-08-14 15:11:16 +0530

    stuff

--- model/src/routes/summarizer.ts
+++ model/src/routes/summarizer.ts
@@ -3,6 +3,6 @@ import { summarizePdf } from "../controllers/summarizer";
 
 const summarizer = new Hono();
 
-summarizer.post("/", summarizePdf);
+summarizer.post("/summarize-pdf", summarizePdf);
 
 export default summarizer;
