ef9d0ab72408c72e20f0e36de669f5b5835eaf4e
Divyam Singh <divyamsingh1918@gmail.com>
2025-08-19 03:58:49 +0530

    dashboard-fixing

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <div className="flex min-h-screen items-center justify-center p-6">
       <div className="w-full max-w-xs flex flex-col gap-6">
-        {/* Logo + Title */}
+       
         <div className="flex justify-center">
           <a href="/" className="flex items-center gap-2 font-medium">
             <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
@@ -15,7 +15,7 @@ export default function LoginPage() {
           </a>
         </div>
 
-        {/* Login Form */}
+         
         <LoginForm />
       </div>
     </div>
